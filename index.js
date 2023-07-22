729860ee455b6d1903f2a6a6cb90564a0a90fe76
lexrayne <lexrayne@outlook.com>
2023-07-21 21:07:40 -0300

    Whitespace change

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ inquirer
             type: 'list',
             name: 'view',
             message: 'What would you like to do?',
-            choices: [  'View All Departments',
-                        'View All Roles',
-                        'View All Employees',
-                        'Add a Department',
-                        'Add a Role',
-                        'Add an Employee',
-                        'Update an Employee Role'   ]
+            choices: ['View All Departments',
+                      'View All Roles',
+                      'View All Employees',
+                      'Add a Department',
+                      'Add a Role',
+                      'Add an Employee',
+                      'Update an Employee Role']
         }
     ])
     .then((data) => {
-        
+
     })
