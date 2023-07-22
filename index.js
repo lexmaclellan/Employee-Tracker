6616ffe9cf0189390f7e48efc602e042bffdcd10
lexrayne <lexrayne@outlook.com>
2023-07-21 21:06:41 -0300

    Added inquirer menu

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const inquirer = require ('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'view',
+            message: 'What would you like to do?',
+            choices: [  'View All Departments',
+                        'View All Roles',
+                        'View All Employees',
+                        'Add a Department',
+                        'Add a Role',
+                        'Add an Employee',
+                        'Update an Employee Role'   ]
+        }
+    ])
+    .then((data) => {
+        
+    })
